fc60fc9052ea10d407351dd3c303430493430cff
darinacosta <darinjacosta@gmail.com>
2015-05-23 23:59:52 -0500

    update records

--- track_airbnbs.js
+++ track_airbnbs.js
@@ -5,7 +5,7 @@ var unirest = require('unirest'),
     geoJson = { "type": "FeatureCollection",
       "features": []};
 
-unirest.get("https://zilyo.p.mashape.com/search?isinstantbook=true&nelatitude=30&nelongitude=-90&provider=airbnb%2Chousetrip&swlatitude=29&swlongitude=-91")
+unirest.get("https://zilyo.p.mashape.com/search?isinstantbook=true&nelatitude=30.123749&nelongitude=-89.868164&provider=airbnb%2Chousetrip&swlatitude=29.881137&swlongitude=-90.557556")
 .header("X-Mashape-Key", "9qRGPp2G8Pmsh63uQFNtIAesIx8cp1ZisvLjsnt2HhMqd6jGu7")
 .header("Accept", "application/json")
 .end(function (result) {
