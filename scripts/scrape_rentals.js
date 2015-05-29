b65cf96e1791fb7cb4df70cc6d95ae1110daf368
darinacosta <darinjacosta@gmail.com>
2015-05-29 08:06:12 -0500

    integrate async

--- scripts/scrape_rentals.js
+++ scripts/scrape_rentals.js
@@ -14,17 +14,24 @@ rentalScraper = {
   	var nelatitude = 30.123749,
         nelongitude = -89.868164,
         swlatitude = 29.881137,
-        swlongitude = -90.557556;
-  
-    console.log('Scanning page ' + i + '...')
-  	unirest.get("https://zilyo.p.mashape.com/search?isinstantbook=false" + "&nelatitude=" + nelatitude + "&nelongitude=" + nelongitude + "&swlatitude=" + swlatitude + "&swlongitude=" + swlongitude + "&&provider=airbnb%2C+alwaysonvacation%2C+apartmentsapart%2C+bedycasa%2C+bookingpal%2C+citiesreference%2C+edomizil%2C+geronimo%2C+gloveler%2C+holidayvelvet%2C+homeaway%2C+homestay%2C+hostelworld%2C+housetrip%2C+interhome%2C+nflats%2C+roomorama%2C+stopsleepgo%2C+theotherhome%2C+travelmob%2C+vacationrentalpeople%2C+vaycayhero%2C+waytostay%2C+webchalet%2C+zaranga&" + "page=" + pageNumber)
+        swlongitude = -90.557556,
+        url = "https://zilyo.p.mashape.com/search?isinstantbook=false" + 
+              "&nelatitude=" + nelatitude + "&nelongitude=" + nelongitude + 
+              "&swlatitude=" + swlatitude + "&swlongitude=" + swlongitude + 
+              "&&provider=airbnb%2C+alwaysonvacation%2C+apartmentsapart%2C+bedycasa%2C+bookingpal%2C+citiesreference%2C+edomizil%2C+geronimo%2C+gloveler%2C+holidayvelvet%2C+homeaway%2C+homestay%2C+hostelworld%2C+housetrip%2C+interhome%2C+nflats%2C+roomorama%2C+stopsleepgo%2C+theotherhome%2C+travelmob%2C+vacationrentalpeople%2C+vaycayhero%2C+waytostay%2C+webchalet%2C+zaranga&" + 
+              "page=" + pageNumber;
+    
+  	unirest.get(url)
   	.header("X-Mashape-Key", scraper_env.mashape_key)
   	.header("Accept", "application/json")
   	.end(function (result) {
-     
+      console.log('Scanning page ' + pageNumber + '...')
   		var parsedResult = JSON.parse(result.body);
-      callback()
-  	  return parsedResult;
+      if (parsedResult['ids'] > 1){
+        callback(parsedResult)
+      } else {
+        return 'Complete';
+      }
     });
   },
   
@@ -34,11 +41,12 @@ rentalScraper = {
     escape = 0,
     results = [];
     rentalScraper.getResponseByPage(i, function(pageResult){
-      console.log('TEEEEST')
+      if (pageResult === 'Complete') return;
       bodyResult = pageResult['result'];
       if (pageResult['ids'] < 1) escape = 1;
       for (var n = 0; n < bodyResult.length; n ++){
         rentalScraper._pushToGeoJson(bodyResult[n])
+        console.log(bodyResult[n])
       } 
     }); 
   },
