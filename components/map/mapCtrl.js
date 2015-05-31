7251b132781e55280f7c432de6d45ac3e45ddba2
darinacosta <darinjacosta@gmail.com>
2015-05-31 15:08:47 -0500

    bust cache on multiunitlayer

--- components/map/mapCtrl.js
+++ components/map/mapCtrl.js
@@ -59,7 +59,7 @@ function mapCtrl($scope, mapSvc, layerHelpers, $http) {
   };
 
 
-  $http.get("./layers/multiUnitRentals.json").success(
+  $http.get("./layers/multiUnitRentals.json?v=0.01").success(
     configureShortTermRentalLayer
   );
   
