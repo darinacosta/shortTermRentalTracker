1c49320a328d049f1b21cfa1f1e6bb9fdedf9621
darinacosta <darinjacosta@gmail.com>
2015-06-11 11:54:38 -0500

    search mutliple providers

--- scripts/modules/rentalScraper.js
+++ scripts/modules/rentalScraper.js
@@ -15,8 +15,15 @@ rentalScraper = {
   _pageCount: 1,
   _numberOfFeaturesWritten: 0,
   _urlList: {urls:[]},
-  fetchListings: function(pageNum) { 
-  	var rentalScraper = this,
+  fetchListings: function(providers) { 
+    var rentalScraper = this;
+    providers.forEach(function(provider){
+      rentalScraper._fetchListingsByProvider(provider);
+    });
+  },
+
+  _fetchListingsByProvider: function(provider){
+    var rentalScraper = this,
         nelatitude = 30.123749,
         nelongitude = -89.868164,
         swlatitude = 29.881137,
@@ -24,26 +31,24 @@ rentalScraper = {
         url = "https://zilyo.p.mashape.com/search?isinstantbook=false" + 
               "&nelatitude=" + nelatitude + "&nelongitude=" + nelongitude + 
               "&swlatitude=" + swlatitude + "&swlongitude=" + swlongitude + 
-              "&&provider=airbnb%2C+alwaysonvacation%2C+apartmentsapart%2C+bedycasa%2C+bookingpal%2C+citiesreference%2C+edomizil%2C+geronimo%2C+gloveler%2C+holidayvelvet%2C+homeaway%2C+homestay%2C+hostelworld%2C+housetrip%2C+interhome%2C+nflats%2C+roomorama%2C+stopsleepgo%2C+theotherhome%2C+travelmob%2C+vacationrentalpeople%2C+vaycayhero%2C+waytostay%2C+webchalet%2C+zaranga&" + 
+              "&&provider=" + provider + "&" +
               "page=" + rentalScraper._pageCount;
     
-  	unirest.get(url)
-  	.header("X-Mashape-Key", config.mashape_key)
-  	.header("Accept", "application/json")
-  	.end(function (result) {
+    unirest.get(url)
+    .header("X-Mashape-Key", config.mashape_key)
+    .header("Accept", "application/json")
+    .end(function (result) {
       console.log('Scanning page ' + rentalScraper._pageCount + '...')
-  		var parsedResult = JSON.parse(result.body);
+      var parsedResult = JSON.parse(result.body);
       rentalScraper._handleResult(parsedResult);
       rentalScraper._pageCount = rentalScraper._pageCount + 1;
-      if (parsedResult['ids'].length > 1 && pageNum === undefined){
-        rentalScraper.fetchListings()
-      }else if (pageNum !== undefined){
-        console.log(pageNum)
-        console.log(parsedResult['result'][1]['provider']['url'])
-      }else {
+      if (parsedResult['ids'].length > 1){
+        rentalScraper._fetchListingsByProvider(provider)
+      } else {
+        rentalScraper._pageCount = 1;
         rentalScraper._writeGeojsonToFile();
       }
-    });
+    })
   },
   
   _handleResult: function(result){
