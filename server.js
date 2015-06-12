eb30a4beed97eea019605e98949c1c3ad112b3ce
unknown <djacosta@ITI-1040L.cityofno.com>
2015-06-12 16:07:23 -0500

    update server

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('*', function(req, res){
    res.sendFile(__dirname + '/public/index.html');
 });
  
-/*var port = 10001;
+var port = process.env.PORT || 3000;
 app.listen(port, function() {
    console.log('server listening on port ' + port);
-});*/
+});
