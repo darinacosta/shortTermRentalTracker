ce8ebe4ba9dec611bc61219630cd3a3e70d30951
unknown <djacosta@ITI-1040L.cityofno.com>
2015-06-16 14:36:07 -0500

    async death merchant

--- scripts/modules/rentalScraper.js
+++ scripts/modules/rentalScraper.js
@@ -108,10 +108,10 @@ rentalScraper = {
   _mapSeries: function(arr, iterator, cb) {
     // create a empty promise to start our series (so we can use `then`)
     var currentPromise = Q();
-    console.log(arr.length);
     var promises = arr.map(function (el) {
       return currentPromise = currentPromise.then(function () {
         // execute the next function after the previous has resolved successfully
+        console.log(el.properties.id + " HI");
         return iterator(el)
       })
     })
