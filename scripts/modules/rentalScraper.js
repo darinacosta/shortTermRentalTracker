a0ce96a2c9883e2f6f1142d12d95c6c33af4812c
unknown <djacosta@ITI-1040L.cityofno.com>
2015-06-16 14:29:57 -0500

    morepromsmasdfddddfg

--- scripts/modules/rentalScraper.js
+++ scripts/modules/rentalScraper.js
@@ -109,7 +109,6 @@ rentalScraper = {
     // create a empty promise to start our series (so we can use `then`)
     var currentPromise = Q();
     var promises = arr.map(function (el) {
-      console.log(el);
       return currentPromise = currentPromise.then(function () {
         // execute the next function after the previous has resolved successfully
         return iterator(el)
@@ -154,6 +153,7 @@ rentalScraper = {
   _writeFeatureToDb: function(feature){
     var deferred = Q.defer();
     MongoClient.connect(rentaldb, function(e, db) {
+      console.log(feature.properties.id + " made it")
       if (db === null){
         console.log('Bad database connection.')
         deferred.resolve();
