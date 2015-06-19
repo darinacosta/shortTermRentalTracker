d86cfd1c3feacee8e8599b75f49723a1bc65dabf
darinacosta <darinjacosta@gmail.com>
2015-06-19 18:16:56 +0000

    create feature server

--- server.js
+++ server.js
@@ -1,16 +1,35 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var http = require('http');
+var url = require('url');
+var mongo = require('mongodb');
+var mdbServer = mongo.Server('localhost', 27017, {'auto_reconnect' : true});
+var mdb = mongo.Db('shorttermrentals', mdbServer);
+var BSON = require('mongodb').BSON;
 
-app.use('/',express.static(__dirname + '/public'));
-app.use('/bower_components',express.static(__dirname + '/bower_components'));
-app.use('/node_modules',express.static(__dirname + '/node_modules'));
+//Routing
 
-app.get('*', function(req, res){
-   res.sendFile(__dirname + '/public/index.html');
+
+app.get('/rentaltracker', function(req, res){
+  res.setHeader('Content-Type', 'application/json'); 
+  var urlParts = url.parse(req.url, true);
+  var queryObject = urlParts.query;
+  var query = queryObject.all === true ? {} : queryObject; 
+  mdb.open(function(err, db){
+     db.collection('features')
+     .find(query)
+     .toArray(function(err,features){
+       res.send({body:features});
+       db.close()
+     });
+   });
 });
- 
+  
+
+//Servers 
 var port = 8080;
-app.listen(port, function() {
+app.listen(port, function(err, res) {
+   if (err) { console.log( err ) };
    console.log('server listening on port ' + port);
 });
