9ec2f71ab5f62197ada83c99855062d663ac0f4f
darinacosta <darinjacosta@gmail.com>
2015-06-20 18:28:52 -0500

    add conditional style rules

--- public/services/layerSvc.js
+++ public/services/layerSvc.js
@@ -1,7 +1,10 @@
 app.factory("layerSvc", ['$http', 'layerHelpers', layerSvc]);
 
-  function layerSvc($http, layerHelpers){
-     function configureLicensedRentals(geojson){
+function layerSvc($http, layerHelpers){
+
+  /*LICENSED RENTALS*/
+
+  function configureLicensedRentals(geojson){
     return L.geoJson(geojson, {
       pointToLayer: styleLicensedRentals,
       onEachFeature: licensedRentalPopup
@@ -18,17 +21,38 @@ app.factory("layerSvc", ['$http', 'layerHelpers', layerSvc]);
       fillOpacity: 0.8,
       name: 'licensed'
     })
-  }
+  };
   
   function licensedRentalPopup(feature, layer) {
     var popup = '<h4>' + feature.properties.Trade_Name + '<br> <small>' + feature.properties.Address + '</small></h4>';
     layer.bindPopup(popup);
   };
+
+  /*SHORT TERM RENTALS TOTAL*/
+
+  function configureShortTermRentalLayer(data, status) {
+    shortTermRentalLayer = L.geoJson(data, {
+      onEachFeature: shortTermRentalPopup,
+      pointToLayer: shortTermRentalPointStyle
+    });
+    //gotta make an extra layer for the cluster or it freaks outs
+    var shortTermRentalClusterLayer = L.geoJson(data, {
+      onEachFeature: shortTermRentalPopup,
+      pointToLayer: shortTermRentalPointStyle
+    });
+    shortTermRentalClusters = new L.MarkerClusterGroup();
+    shortTermRentalClusters.addLayer(shortTermRentalClusterLayer);
+    
+    return {
+      shortTermRentalClusters: shortTermRentalClusters, 
+      shortTermRentalLayer: shortTermRentalLayer
+    };
+  };
   
   function shortTermRentalPointStyle(feature, latlng) {
     return L.circleMarker(latlng, {
       radius: 4,
-      fillColor: colorPointsConditionally(feature),
+      fillColor: colorTotalPointsConditionally(feature),
       color: "#000",
       weight: 1,
       opacity: 1,
@@ -37,16 +61,12 @@ app.factory("layerSvc", ['$http', 'layerHelpers', layerSvc]);
     })
   };
   
-  function colorPointsConditionally(feature){
-    unitNumber = parseInt(feature.properties.units)
-    if (unitNumber > 1 && unitNumber < 5){
-      return "#FF7000";
-    }else if (unitNumber > 4 && unitNumber < 20){
-      return "#FF4000"
-    }else if (unitNumber > 20){ 
-      return "#FF0000"
-    }else{
-      return "#FFE000"
+  function colorTotalPointsConditionally(feature){
+    provider = feature.properties.provider;
+    if (provider.substring(0,3) === "air"){
+      return "#B32B2B";
+    } else if (provider.substring(0,1) === "hma") {
+      return "#2B5CB3";
     }
   };
   
@@ -71,24 +91,18 @@ app.factory("layerSvc", ['$http', 'layerHelpers', layerSvc]);
     }
     layer.bindPopup(popup);
   };
-  
-  function configureShortTermRentalLayer(data, status) {
-    shortTermRentalLayer = L.geoJson(data, {
-      onEachFeature: shortTermRentalPopup,
-      pointToLayer: shortTermRentalPointStyle
-    });
-    //gotta make an extra layer for the cluster or it freaks outs
-    var shortTermRentalClusterLayer = L.geoJson(data, {
-      onEachFeature: shortTermRentalPopup,
-      pointToLayer: shortTermRentalPointStyle
-    });
-    shortTermRentalClusters = new L.MarkerClusterGroup();
-    shortTermRentalClusters.addLayer(shortTermRentalClusterLayer);
-    
-    return {
-      shortTermRentalClusters: shortTermRentalClusters, 
-      shortTermRentalLayer: shortTermRentalLayer
-    };
+
+  function colorAirbnbsConditionally(feature){
+    unitNumber = parseInt(feature.properties.units)
+    if (unitNumber > 1 && unitNumber < 5){
+      return "#FF7000";
+    }else if (unitNumber > 4 && unitNumber < 20){
+      return "#FF4000"
+    }else if (unitNumber > 20){ 
+      return "#FF0000"
+    }else{
+      return "#FFE000"
+    }
   };
   
   function asyncHelper(callback){
@@ -101,19 +115,33 @@ app.factory("layerSvc", ['$http', 'layerHelpers', layerSvc]);
 
 
   return layerSvc = {
+
     getLicensedRentals: function(){
     	return $http.get("./layers/licensed-rentals.json")
     	.then(function(res){
         return configureLicensedRentals(res.data)
       });
     },
-     getShortTermRentals: function(){
+
+    getShortTermRentals: function(){
       return $http.get("http://54.152.46.39/rentaltracker").then(function(res){
         var geojson = {
           "type": "FeatureCollection",
           "features": res.data.body
         };
+        var airbnbGeojson
         return configureShortTermRentalLayer(geojson);
+      });
+    },
+
+    getAirbnbs: function(){
+      return $http.get("http://54.152.46.39/rentaltracker?provider=air").then(function(res){
+        var geojson = {
+          "type": "FeatureCollection",
+          "features": res.data.body
+        };
+        var airbnbGeojson
+        return configureAirbnbs(geojson);
       });
     }
   }
