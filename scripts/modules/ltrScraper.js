754db6de23a0d6637b618864bda0c319b089fb77
darinacosta <darinjacosta@gmail.com>
2015-06-26 14:22:21 +0000

    modify features

--- scripts/modules/ltrScraper.js
+++ scripts/modules/ltrScraper.js
@@ -1,11 +1,15 @@
 var request = require('request');
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
+var gju = require('geojson-utils');
 var mapSeries = require('promise-map-series');
+var neighborhoodGeojson = require('../layers/neighborhoods.geojson'); 
+var path = require('path');
 var Q = require('q');
 var today = new Date();
 var ltrdb = 'mongodb://localhost:27017/longtermrentals';
 
+var ltrScraper = {};
 
 var config = {
   token: "6467f98a0c3d0cf2abd6dc81e7d0817d",
@@ -22,8 +26,10 @@ var counter = {
   feature: 0
 };
 
-function search(){
-  
+ltrScraper.search = function(){
+ 
+  var ltrScraper= this;
+
   var url = "http://search.3taps.com?" +
  	    "auth_token=" + config.token + 
 	    "&location.county=" + config.county +
@@ -41,22 +47,21 @@ function search(){
       counter.tier += 1
     }
     for (var i = 0; i < postings.length; i++){
-      var feature = buildFeature(postings[i]);
+      var feature = ltrScraper.buildFeature(postings[i]);
       features.push(feature);
     };
-    mapSeries(features, writeToDb)
+    mapSeries(features, ltrScraper.writeToDb)
     .then(function(){
-      search();
+     ltrScraper.search();
     })
   });
 };
 
-
-function writeToDb(feature){   
+ltrScraper.writeToDb = function(feature){   
   var deferred = Q.defer();
   MongoClient.connect(ltrdb, function(e, db){
     setTimeout(function(){
-      config.feature += 1;
+      counter.feature += 1;
       console.log("Feature: " + counter.feature + " | ID: " + feature.properties.id + " | Page: " + counter.page + " | Tier: " + counter.tier);  
       db.collection('features').find({"properties.id": feature.properties['id']}).count(function(e,n){
         if (n === 0){
@@ -77,23 +82,38 @@ function writeToDb(feature){
   return deferred.promise;
 };
 
-function buildFeature(listing){
+ltrScraper.buildFeature = function(listing){
+  var coords =  [listing.location['long'], listing.location['lat']];
+  var neighborhood = this.getNeighborhood(coords);
   return {
-    type: "Feature",
-    geometry: {
-      type: "Point",
-      coordinates: [listing.location['long'], listing.location['lat']]
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": coords
     },
-    properties: {
-      id: listing['id'],
-      account_id: listing['account_id'],
-      city: listing.location['city'],
-      zip: listing.location['zipcode'],
-      heading: listing['heading'],
-      currency: listing['currency'],
-      price: listing['price'] || 0
+    "properties": {
+      "id": listing['id'],
+      "account_id": listing['account_id'],
+      "city": listing.location['city'],
+      "zip": listing.location['zipcode'],
+      "heading": listing['heading'],
+      "currency": listing['currency'],
+      "price": listing['price'] || 0,
+      "neighborhood": neighborhood,
+      "url": listing['external_url']
     }
   } 
 };
 
-search();
+ltrScraper.getNeighborhood = function(coords){
+  var point = {"type": "Point", "coordinates": coords};
+  for (var i = 0; i < neighborhoodGeojson.features.length; i ++){
+    var neighborhood = neighborhoodGeojson.features[i];
+    var poly = {"type": "Polygon", "coordinates": neighborhood.geometry.coordinates[0]};
+    var inPoly = gju.pointInPolygon(point, poly);
+    if (inPoly !== false){ return neighborhood.properties.neighbourhood };
+  }
+}
+
+ltrScraper.search();
+//module.exports = ltrScraper;
