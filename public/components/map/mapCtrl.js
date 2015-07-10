c576ac6af571c42db70e79cf30ea5810e07e4909
darinacosta <darinjacosta@gmail.com>
2015-07-09 21:16:58 -0500

    move layerSvc to mapCtrl

--- public/components/map/mapCtrl.js
+++ public/components/map/mapCtrl.js
@@ -1,6 +1,6 @@
-app.controller('mapCtrl', ['$scope', '$rootScope', '$q', '$timeout', '$http', 'layerSvc', mapCtrl]);
+app.controller('mapCtrl', ['$scope', '$rootScope', '$q', '$timeout', '$http', mapCtrl]);
 
-function mapCtrl($scope, $rootScope, $q, $timeout, $http, layerSvc) {
+function mapCtrl($scope, $rootScope, $q, $timeout, $http) {
   var shortTermRentalLayer = {},
     shortTermRentalClusters = {};
     $scope.legend = "";
@@ -9,6 +9,172 @@ function mapCtrl($scope, $rootScope, $q, $timeout, $http, layerSvc) {
     attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
     maxZoom: 16
   });
+
+  function configureLicensedRentals(geojson){
+    var licensedRentals = L.geoJson(geojson, {
+      pointToLayer: styleLicensedRentals,
+      onEachFeature: licensedRentalPopup
+    });
+
+    return licensedRentals;
+  };
+  
+  function styleLicensedRentals(feature,latlng){
+    return L.circleMarker(latlng, {
+      radius: 4,
+      fillColor: "lightblue",
+      color: "#000",
+      weight: 1,
+      opacity: 1,
+      fillOpacity: 0.8,
+      name: 'licensed'
+    })
+  };
+    
+  function licensedRentalPopup(feature, layer) {
+    var popup = '<h4>' + feature.properties.Trade_Name + '<br> <small>' + feature.properties.Address + '</small></h4>';
+    layer.bindPopup(popup);
+  };
+
+  /*SHORT TERM RENTALS TOTAL*/
+
+  function configureShortTermRentalLayer(data, status) {
+    shortTermRentalLayer = L.geoJson(data, {
+      onEachFeature: shortTermRentalPopup,
+      pointToLayer: shortTermRentalPointStyle
+    });
+    //gotta make an extra layer for the cluster or it freaks outs
+    var shortTermRentalClusterLayer = L.geoJson(data, {
+      onEachFeature: shortTermRentalPopup,
+      pointToLayer: shortTermRentalPointStyle
+    });
+    shortTermRentalClusters = new L.MarkerClusterGroup();
+    shortTermRentalClusters.addLayer(shortTermRentalClusterLayer);
+     
+    leafletData.getMap().then(function(map) {
+      map.addLayer(shortTermRentalClusters);
+      map.addLayer(shortTermRentalLayer);
+    });
+  };
+  
+  function shortTermRentalPointStyle(feature, latlng) {
+    return L.circleMarker(latlng, {
+      radius: 4,
+      fillColor: colorTotalPointsConditionally(feature),
+      color: "#000",
+      weight: 1,
+      opacity: 1,
+      fillOpacity: 0.8,
+      name: 'shortterm'
+    })
+  };
+  
+  function colorTotalPointsConditionally(feature){
+    provider = feature.properties.provider;
+    if (provider.substring(0,3) === "air"){
+      return "#B32B2B";
+    } else if (provider.substring(0,3) === "hma") {
+      return "#ABA925";
+    }
+  };
+  
+  function shortTermRentalPopup(feature, layer) {
+    var popup;
+    if (feature.properties.user !== undefined && feature.properties.provider === "air"){
+      var pluralListing = feature.properties.units === '1' ? 'listing' : 'listings',
+      userUrl = feature.properties.user,
+      userUrlArray = userUrl.split('/'),
+      userId = userUrlArray[userUrlArray.length -1];
+      popup = '<h4>' + feature.properties.street + ' Rental<br> <small>' + feature.properties.roomtype + ' | ' + feature.properties.reviews + ' reviews</small></h5>' +
+              '<b>Rental:</b> <a target="_blank" href="' + feature.properties.url + '">' + feature.properties.url + '</a><br>' +
+              '<b>User Profile:</b> <a target="_blank" href="' + userUrl + '">' + userUrl + '</a><br>' + 
+              '<b>User ID:</b> ' + userId + '<br><br>' + 
+              '<b>This user has ' + feature.properties.units + ' ' + pluralListing + '.</b>';
+    } else if (feature.properties.reviews !== undefined && feature.properties.provider === "hma"){ 
+      userName = feature.properties.user,
+      popup = '<h4>' + feature.properties.street + ' Rental<br> <small>' + feature.properties.roomtype + ' | ' + feature.properties.reviews + ' reviews</small></h5>' +
+              '<b>Rental:</b> <a target="_blank" href="' + feature.properties.url + '">' + feature.properties.url + '</a><br>' +
+              '<b>Name provided by user:</b> ' + userName + '<br><br>'  
+    } else {
+      popup = '<h3>' + feature.properties.street + ' Rental<br> <small>(' + feature.properties.roomtype + ')</small></h3>' +
+              '<b>Rental:</b> <a target="_blank" href="' + feature.properties.url + '">' + feature.properties.url + '</a><br>';
+    }
+    layer.bindPopup(popup);
+  };
+
+  /*Airbnb Layer*/ 
+  
+  function configureAirbnbs(geojson){
+    return L.geoJson(geojson, {
+      pointToLayer: airbnbPointStyle,
+      onEachFeature: shortTermRentalPopup
+    })
+  };
+
+  function airbnbPointStyle(feature, latlng) {
+    return L.circleMarker(latlng, {
+      radius: 4,
+      fillColor: colorAirbnbsConditionally(feature),
+      color: "#000",
+      weight: 1,
+      opacity: 1,
+      fillOpacity: 0.8,
+      name: 'shortterm'
+    })
+  };
+
+  function colorAirbnbsConditionally(feature){
+    unitNumber = parseInt(feature.properties.units)
+    if (unitNumber > 1 && unitNumber < 5){
+      return "#FF7000";
+    }else if (unitNumber > 4 && unitNumber < 20){
+      return "#FF4000"
+    }else if (unitNumber > 20){ 
+      return "#FF0000"
+    }else{
+      return "#FFE000"
+    }
+  };
+  
+  function asyncHelper(callback){
+    $timeout(function(){
+      $scope.$apply(
+        callback()
+      )
+    });
+  };
+
+
+  return layerSvc = {
+
+    getLicensedRentals: function(){
+      return $http.get("./layers/licensed-rentals.json")
+      .then(function(res){
+        return configureLicensedRentals(res.data)
+      });
+    },
+
+    getShortTermRentals: function(){
+      return $http.get("http://54.152.46.39/rentaltracker?userexists=true&neworleans=true").then(function(res){
+        var geojson = {
+          "type": "FeatureCollection",
+          "features": res.data.body
+        };
+        var airbnbGeojson
+        return configureShortTermRentalLayer(geojson);
+      });
+    },
+
+    getAirbnbs: function(){
+      return $http.get("http://54.152.46.39/rentaltracker?provider=air").then(function(res){
+        var geojson = {
+          "type": "FeatureCollection",
+          "features": res.data.body
+        };
+        return configureAirbnbs(geojson);
+      });
+    }
+  };
 
   angular.extend($scope, {
     defaults: {
@@ -24,9 +190,12 @@ function mapCtrl($scope, $rootScope, $q, $timeout, $http, layerSvc) {
       lat: 29.970996, 
       lng: -90.058537,
       zoom: 12
+    },
+    layers: {
+      baselayers: {
+        licensedRentals: layerSvc.getLicensedRentals(),
+        shortTermRentals: layerSvc.getShortTermRentals()
+      }
     }
   });
-
-  layerSvc.getLicensedRentals();
-  layerSvc.getShortTermRentals();
 }
