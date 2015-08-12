0cd382a3bee1554ca0adb36e985ecddaa9512998
darinacosta <darinjacosta@gmail.com>
2015-08-12 17:13:12 +0000

    increase max zoom

--- public/services/mapSvc.js
+++ public/services/mapSvc.js
@@ -15,7 +15,7 @@ function mapSvc($http){
 
   Esri_WorldGrayCanvas = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
     attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
-    maxZoom: 15
+    maxZoom: 16
   });
 
   //Zoom Home Bar
