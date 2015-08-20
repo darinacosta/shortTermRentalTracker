7a55b748688171f60ce003eeb5cc803074dbdcf5
darinacosta <darinjacosta@gmail.com>
2015-08-20 16:04:33 -0500

    charts

--- public/components/home/homeCtrl.js
+++ public/components/home/homeCtrl.js
@@ -8,5 +8,33 @@ function homeCtrl($scope, mapSvc, layerHelpers, $http) {
     shortTermRentalClustersManager = new layerManager("Regional Short Term Rental Clusters"),
     shortTermRentalClusters;
 
+  $scope.airbnbTotal = '----';
+  
+  $http.get("../../assets/data/stats.json").success(function(data){
+    console.log(data); 
+    
+    $scope.airbnbTotal = data.listingTotals.air;
+
+    //Room Type Chart 
+    $scope.rentalTypeLabels = ["Entire home/apt", "Shared Room", "Private Room"];
+    $scope.rentalTypeData = [data.roomTypeTotals.entireHome, data.roomTypeTotals.sharedRoom, data.roomTypeTotals.privateRoom];
+    
+    $scope.chartColors = [{
+      fillColor: 'red',
+      strokeColor: 'red',
+      highlightFill: 'grey',
+      highlightStroke: 'grey'
+    }];
+  });
+
   map.setView(mapAttributes.center, mapAttributes.zoom);
 }
+
+//HELPER FUNCTIONS MOVE THESE TO A SERVICE
+function asyncHelper(callback){
+  $timeout(function(){
+    $scope.$apply(
+      callback()
+    )
+  });
+};
