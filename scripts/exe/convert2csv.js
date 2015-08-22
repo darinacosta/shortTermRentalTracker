2e8b89d91c70487647e44a20e0b1f87f9a615874
darinacosta <darinjacosta@gmail.com>
2015-08-21 22:58:26 -0500

    fix csv

--- scripts/exe/convert2csv.js
+++ scripts/exe/convert2csv.js
@@ -1,8 +1,8 @@
 csvConverter = require('./../modules/csvConverter');
 
-csvConverter.convert2csv('air','airbnb.txt');
+csvConverter.convert2csv('air','airbnb.csv');
 
 //Wait before making another request to the server
 setTimeout(function(){
-  csvConverter.convert2csv('hma','homeaway.txt')
-}, 2000);	
+  csvConverter.convert2csv('hma','homeaway.csv')
+}, 1000);	
--- scripts/modules/csvConverter.js
+++ scripts/modules/csvConverter.js
@@ -4,13 +4,13 @@ var http = require('http');
 var path = require('path');
 var csvConverter = {};
 
-csvConverter.airFields = ['properties.id','geometry.coordinates','properties.city','properties.updated','properties.nightlyprice','properties.monthlyprice','properties.neighborhood','properties.roomtype','properties.street','properties.units','properties.url','properties.user'];
+csvConverter.airFields = ['properties.id','properties.x', 'properties.y', 'properties.city','properties.updated','properties.nightlyprice','properties.monthlyprice','properties.neighborhood','properties.roomtype','properties.street','properties.units','properties.url','properties.user'];
 
-csvConverter.airFieldNames = ['ID', 'Coordinates', 'City', 'Date Collected', 'Nightly Price', 'Monthly Price', 'Neighborhood', 'Room Type', 'Street', 'Units', 'URL', 'User URL'];
+csvConverter.airFieldNames = ['ID', 'X', 'Y', 'City', 'Date Collected', 'Nightly Price', 'Monthly Price', 'Neighborhood', 'Room Type', 'Street', 'Units', 'URL', 'User URL'];
 
-csvConverter.hmaFields = ['properties.id','geometry.coordinates','properties.city','properties.updated','properties.nightlyprice','properties.monthlyprice','properties.neighborhood','properties.roomtype','properties.street','properties.url','properties.user'];
+csvConverter.hmaFields = ['properties.id','properties.x','properties.y','properties.city','properties.updated','properties.nightlyprice','properties.monthlyprice','properties.neighborhood','properties.roomtype','properties.street','properties.url','properties.user'];
 
-csvConverter.hmaFieldNames = ['ID', 'Coordinates', 'City', 'Date Collected', 'Nightly Price', 'Monthly Price', 'Neighborhood', 'Room Type', 'Street', 'URL', 'Name Provided'];
+csvConverter.hmaFieldNames = ['ID', 'X', 'Y', 'City', 'Date Collected', 'Nightly Price', 'Monthly Price', 'Neighborhood', 'Room Type', 'Street', 'URL', 'Name Provided'];
 
 csvConverter.convert2csv = function(provider, filename) {
   http.get({
@@ -23,14 +23,24 @@ csvConverter.convert2csv = function(provider, filename) {
     });
     res.on('end', function(){
       var parsed = JSON.parse(body);
-    
+      
+      for (var i = 0; i < parsed.body.length; i++){
+      	var listing = parsed.body[i];
+	listing.properties.x = listing.geometry.coordinates[0];
+        listing.properties.y = listing.geometry.coordinates[1];
+	if (listing.properties.provider === 'hma'){
+          listing.properties['user'] = listing.properties['user'] !== null && typeof(listing.properties['user']) === 'object' ? listing.properties['user'][0] : listing.properties['user'];
+	}
+      };
+
       var csvOptions = {
         data: parsed.body,
         fields: provider === "air" ? csvConverter.airFields : csvConverter.hmaFields,
         fieldNames: provider === "air" ? csvConverter.airFieldNames : csvConverter.hmaFieldNames,
         nested: true,
-        del: ';'
+        del: ','
       };
+
       json2csv(csvOptions, function(err, csv){
         if (err) console.log(err);
         fs.writeFile(path.join(__dirname, '../../public/assets/' + filename), csv, function(err){
